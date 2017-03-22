e326820033dc79f28af3822efb9a6503e7bfafb9
index188 <index188@gmail.com>
2017-03-22 11:34:10 -0400

    fixed!

--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,6 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,7 +9,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
